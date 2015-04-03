9c2f34d56d5f537ffccc6485675dba0bbe528917
minaatefmaf <minaatefmaf@gmail.com>
2015-04-03 13:44:17 +0200

    Create our Model of the recorded audio.

---
+++ Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,21 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Mina Atef on 4/3/15.
+//  Copyright (c) 2015 minaatefmaf. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject {
+    
+    var filePathUrl: NSURL!
+    var title: String!
+    
+    init(filePathUrl: NSURL!, title: String!) {
+        self.filePathUrl = filePathUrl
+        self.title = title
+    }
+    
+}
