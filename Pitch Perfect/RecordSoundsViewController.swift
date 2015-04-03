d2e27287ace26d4e6c4126758c02a11ae7224e4d
minaatefmaf <minaatefmaf@gmail.com>
2015-04-03 13:09:50 +0200

    Add the functionality of recording to the record button.

--- Pitch Perfect/RecordSoundsViewController.swift
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class RecordSoundsViewController: UIViewController {
 
     @IBOutlet weak var labelBelowRecordingButton: UILabel!
     @IBOutlet weak var stopButton: UIButton!
     @IBOutlet weak var recordButton: UIButton!
+
+    var audioRecorder: AVAudioRecorder!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,11 +39,35 @@ class RecordSoundsViewController: UIViewController {
         stopButton.hidden = false
         // Disabling the record button so that the user of our app will now not be able to accidentally press the record button twice.
         recordButton.enabled = false
+        // Record the user's voice
+        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+        
+        let currentDateTime = NSDate()
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "ddMMyyyy-HHmmss"
+        let recordingName = formatter.stringFromDate(currentDateTime)+".wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        println(filePath)
+        
+        // Setup audio session
+        var session = AVAudioSession.sharedInstance()
+        session.setCategory(AVAudioSessionCategoryPlayAndRecord, error: nil)
+        
+        // Initialize and prepare the recorder
+        audioRecorder = AVAudioRecorder(URL: filePath, settings: nil, error: nil)
+        audioRecorder.meteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
     }
 
     @IBAction func stopButton(sender: UIButton) {
         // Hide the "recording" text
         labelBelowRecordingButton.hidden = true
+        // Stop recording the user's voice
+        audioRecorder.stop()
+        var audioSession = AVAudioSession.sharedInstance()
+        audioSession.setActive(false, error: nil)
     }
     
 }
