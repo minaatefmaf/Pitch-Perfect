c51942eee478b34f30b87aa25fb54ff26f83efb8
minaatefmaf <minaatefmaf@gmail.com>
2017-02-24 11:52:33 +0200

    chore: Remove the "directory not found for option '-F/..." warning
    Done by deleting the files in "Framework Search Paths" as in:
    http://stackoverflow.com/questions/32678949/swift-2-ios-9-ld-warning-directory-not-found-iphonesimulator9-0-sdk-develope
    & http://stackoverflow.com/questions/30827022/xcode-7-library-search-path-warning/32620919#32620919

---
+++ Pitch Perfect Tests/Pitch_Perfect_Tests.swift
@@ -0,0 +1,35 @@
+//
+//  Pitch_Perfect_Tests.swift
+//  Pitch Perfect Tests
+//
+//  Created by Mina Atef on 2/23/17.
+//  Copyright © 2017 minaatefmaf. All rights reserved.
+//
+
+import XCTest
+
+class Pitch_Perfect_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
